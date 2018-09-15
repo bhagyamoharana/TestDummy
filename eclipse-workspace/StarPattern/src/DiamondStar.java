95136f8187754a4ff5ae89e61a316033a208dc0b
bhagyamoharana <43140436+bhagyamoharana@users.noreply.github.com>
2018-09-15 21:19:55 +0100

    FirstCommit from eclipse

---
+++ eclipse-workspace/StarPattern/src/DiamondStar.java
@@ -0,0 +1,23 @@
+
+public class DiamondStar {
+	public static void main(String[] args) {
+		int i,j,k,l;
+		for( i=1;i<=5;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(k=1;k<=5;k++)
+		{
+			for(l=5;l>=k;l--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
